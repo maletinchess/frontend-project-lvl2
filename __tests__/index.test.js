4620312b109465e482d18656c9d7a3b999be38cb
Pavel Maletin <maletinchess>
2020-07-21 10:30:56 +0700

    update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,18 @@
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import getJsonDiff from '../index';
 
-const firstJson = '/home/pavel/frontend-project-lvl2/__fixtures__/file1.json';
-const secondJson = '/home/pavel/frontend-project-lvl2/__fixtures__/file2.json';
-const expectedFile = '/home/pavel/frontend-project-lvl2/__fixtures__/expected_file.json';
+/* eslint-disable no-underscore-dangle */
+const __filename = fileURLToPath(import.meta.url);
+/* eslint-disable no-underscore-dangle */
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const firstJson = getFixturePath('file1.json');
+const secondJson = getFixturePath('file2.json');
+const expectedFile = getFixturePath('expected_file.json');
 
 test('getJsonDiff', () => {
   expect(getJsonDiff(firstJson, secondJson)).toEqual(fs.readFileSync(expectedFile, 'utf-8'));

4d3502af2c1965f4d68e9628107d51b313f356eb
Pavel Maletin <maletinchess>
2020-08-20 23:31:17 +0700

    9th step update diff

--- diff.js
+++ diff.js
@@ -20,44 +20,44 @@ const getDiff = (obj1, obj2) => {
   const keys = _.union(_.keys(obj1), _.keys(obj2)).sort();
   const callback = (acc, key) => {
     if (analizeKeyModification(obj1, obj2, key) === 'add') {
-      const diff = {};
-      diff.name = `${key}`;
-      diff.stage = 'add';
-      diff.value = obj2[key];
-      diff.type = 'plain';
-      acc.push(diff);
+      acc.push({
+        name: `${key}`,
+        stage: 'add',
+        value: obj2[key],
+        type: 'plain',
+      });
     }
     if (analizeKeyModification(obj1, obj2, key) === 'removed') {
-      const diff = {};
-      diff.name = `${key}`;
-      diff.stage = 'removed';
-      diff.value = obj1[key];
-      diff.type = 'plain';
-      acc.push(diff);
+      acc.push({
+        name: `${key}`,
+        stage: 'removed',
+        value: obj1[key],
+        type: 'plain',
+      });
     }
     if (analizeKeyModification(obj1, obj2, key) === 'unchanged') {
-      const diff = {};
-      diff.name = `${key}`;
-      diff.stage = 'unchanged';
-      diff.value = obj1[key];
-      diff.type = 'plain';
-      acc.push(diff);
+      acc.push({
+        name: `${key}`,
+        stage: 'unchanged',
+        value: obj1[key],
+        type: 'plain',
+      });
     }
     if (analizeKeyModification(obj1, obj2, key) === 'changed') {
-      const diff = {};
-      diff.name = `${key}`;
-      diff.stage = 'updated';
-      diff.value = { before: obj1[key], after: obj2[key] };
-      diff.type = 'plain';
-      acc.push(diff);
+      acc.push({
+        name: `${key}`,
+        stage: 'updated',
+        value: { before: obj1[key], after: obj2[key] },
+        type: 'plain',
+      });
     }
     if (analizeKeyModification(obj1, obj2, key) === 'analize deep') {
-      const diff = {};
-      diff.name = `${key}`;
-      diff.stage = 'deep';
-      diff.children = getDiff(obj1[key], obj2[key]);
-      diff.type = 'node';
-      acc.push(diff);
+      acc.push({
+        name: `${key}`,
+        stage: 'deep',
+        children: getDiff(obj1[key], obj2[key]),
+        type: 'node',
+      });
     }
     return acc;
   };

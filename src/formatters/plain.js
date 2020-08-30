5a587434dc516a62ef01ffc50e0302df143f5d99
Pavel Maletin <maletinchess>
2020-08-30 22:37:53 +0700

    refact plain (remove first filter)

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,34 +10,33 @@ const normalizeValue = (value) => {
   return `'${value}'`;
 };
 
-const formatPlain = (tree) => tree.filter(({ type }) => type !== 'unchanged')
-  .map((node) => {
+const formatPlain = (tree) => tree.map((node) => {
+  const {
+    name,
+  } = node;
+  const iter = (node1, acc) => {
     const {
-      name,
-    } = node;
-    const iter = (node1, acc) => {
-      const {
-        type, value, valueBefore, valueAfter, children,
-      } = node1;
-      switch (type) {
-        case 'add':
-          return `Property '${acc}' was added with value: ${normalizeValue(value)}`;
-        case 'removed':
-          return `Property '${acc}' was removed`;
-        case 'updated':
-          return `Property '${acc}' was updated. From ${normalizeValue(valueBefore)} to ${normalizeValue(valueAfter)}`;
-        case 'unchanged':
-          return null;
-        case 'nested':
-          return children
-            .map((item) => iter(item, `${acc}.${item.name}`))
-            .filter((item) => item !== null)
-            .join('\n');
-        default:
-          throw new Error(`unknown type: ${type}`);
-      }
-    };
-    return iter(node, name);
-  }).join('\n');
+      type, value, valueBefore, valueAfter, children,
+    } = node1;
+    switch (type) {
+      case 'add':
+        return `Property '${acc}' was added with value: ${normalizeValue(value)}`;
+      case 'removed':
+        return `Property '${acc}' was removed`;
+      case 'updated':
+        return `Property '${acc}' was updated. From ${normalizeValue(valueBefore)} to ${normalizeValue(valueAfter)}`;
+      case 'unchanged':
+        return null;
+      case 'nested':
+        return children
+          .map((item) => iter(item, `${acc}.${item.name}`))
+          .filter((item) => item !== null)
+          .join('\n');
+      default:
+        throw new Error(`unknown type: ${type}`);
+    }
+  };
+  return iter(node, name);
+}).join('\n');
 
 export default formatPlain;
